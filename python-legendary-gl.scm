fbe8e629c1c2a515f8548381143a74096d9dfbd6
Mark Nikkel <mnikkel@gmail.com>
2023-12-08 15:30:13 -0500

    legendary 0.20.33

--- python-legendary-gl.scm
+++ python-legendary-gl.scm
@@ -11,21 +11,19 @@
 (define-public python-legendary-gl
   (package
    (name "python-legendary-gl")
-   (version "0.20.32")
+   (version "0.20.33")
    (source
     (origin
      (method url-fetch)
      (uri (pypi-uri "legendary-gl" version))
      (sha256
       (base32
-       "0mwwfky3xn2scrvf61jhgcc52bip6r705skayzw32jiqqhjbglpa"))))
+       "0c0ym139pn5yc80hc0ynx2am7qxlz0s1w3dgk2zqn7wjf8wc1lm4"))))
    (build-system python-build-system)
    (arguments
     `(#:tests? #f))
-   (propagated-inputs (list
-		       python-requests
-		       python-setuptools
-		       python-wheel))
+   (propagated-inputs (list python-filelock python-requests python-setuptools
+                            python-wheel))
    (home-page "https://github.com/derrod/legendary")
    (synopsis
     "Free and open-source replacement for the Epic Games Launcher application")
