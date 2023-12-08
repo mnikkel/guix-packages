7191cb2b1d9bc8d8e3fd2d42ee4db8fedbcfa1f6
Mark Nikkel <mnikkel@gmail.com>
2023-12-08 15:35:17 -0500

    legendary 0.20.34

--- python-legendary-gl.scm
+++ python-legendary-gl.scm
@@ -11,14 +11,14 @@
 (define-public python-legendary-gl
   (package
    (name "python-legendary-gl")
-   (version "0.20.33")
+   (version "0.20.34")
    (source
     (origin
      (method url-fetch)
      (uri (pypi-uri "legendary-gl" version))
      (sha256
       (base32
-       "0c0ym139pn5yc80hc0ynx2am7qxlz0s1w3dgk2zqn7wjf8wc1lm4"))))
+       "04cn1sv5ks0cy085r9i26gddajxcc6mbqvygxlz1cp6dhbm8slis"))))
    (build-system python-build-system)
    (arguments
     `(#:tests? #f))
